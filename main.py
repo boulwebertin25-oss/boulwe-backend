a03e04ca822b5632daf67d13e08c0db15296dcc2
boulwebertin25-oss <boulwebertin25@gmail.com>
2026-08-30 10:29:03 +0100

    Add 'form' import to main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ Lancer en local avec :
     uvicorn main:app --reload
 """
 
-from fastapi import FastAPI, UploadFile, File
+from fastapi import FastAPI, UploadFile, File,form
 from dotenv import load_dotenv
 from fastapi.middleware.cors import CORSMiddleware
 
